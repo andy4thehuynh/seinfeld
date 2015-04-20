5f6ca247c7f2c77964302e899e764a19e13e0027
Andy Huynh <andy4thehuynh@gmail.com>
2015-04-19 21:13:37 -0700

    ALH: Model layer + API

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-// our app's namespace
+//////////////////////////////////////////
+//
+//
+//  Namespace
+//
+//
+//////////////////////////////////////////
+
 var chain = {};
 
-// model goes here
 
-// controller goes here
 
-// view
+//////////////////////////////////////////
+//
+//
+//  Model
+//
+//
+//////////////////////////////////////////
+
+chain.save = function(list) {
+  localStorage["seinfeld.list"] = JSON.stringify(list);
+};
+chain.load = function() {
+  return JSON.parse(localStorage["seinfeld.list"] || "[]")
+};
+
+// a date model API
+chain.today = function() {
+  var now = new Date();
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+}
+chain.resetDate = function() {
+  return localStorage["seinfeld.start-date"] = chain.today().getTime();
+}
+chain.startDate = function() {
+  return new Date(parseInt(localStorage["seinfeld.start-date"] || chain.resetDate()));
+}
+chain.dateAt = function(index) {
+  var date = new Date(chain.startDate());
+  date.setDate(date.getDate() + index);
+  return date;
+}
+
+
+
+//////////////////////////////////////////
+//
+//
+//  Controller
+//
+//
+//////////////////////////////////////////
+
+
+
+//////////////////////////////////////////
+//
+//
+//  View
+//
+//
+//////////////////////////////////////////
+
 chain.view = function(ctrl) {
   return m("table", chain.seven(function() {
     return m("tr", chain.seven(function() {
@@ -15,3 +71,57 @@ chain.view = function(ctrl) {
     }));
   }));
 };
+
+
+
+//////////////////////////////////////////
+//
+//
+//  Utility Helpers
+//
+//
+//////////////////////////////////////////
+
+chain.seven = function(subject) {
+  var output = [];
+  for (var i = 0; i < 7; i++) output.push(subject(i));
+  return output;
+};
+
+
+
+//////////////////////////////////////////
+//
+//
+//  Execution
+//
+//
+//////////////////////////////////////////
+
+m.module(document.body, { controller: function() {}, view: chain.view });   // render it
+
+
+
+//////////////////////////////////////////
+//
+//
+//  Seinfeld API
+//
+//
+//////////////////////////////////////////
+
+// var list = chain.load();                                  // Basic usage
+// list[42] = true;
+// chain.save(list);
+//
+// chain.save([]);                                           // Reset the list
+//
+//
+//                                                           // Date Model API:
+// var today = chain.today()                                 // today at midnight
+//
+// var startDate = chain.startDate();                        // start date is today
+//
+// var isToday = chain.dateAt(3).getTime() == chain.today()  // is three days from now the same as today? Should be false
+//
+// var newStartDate = chain.resetDate();                     // new start date is today
